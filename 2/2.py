2740e7e9951bc619e6e1363721bcbf736e5d41e5
Henelik <kelyn@crandall.me>
2023-06-24 14:12:16 -0500

    fix problem 2

--- 2/2.py
+++ 2/2.py
@@ -1,4 +1,5 @@
 fib = [1, 1]
+results = []
 
 # generate the list of fibonacci numbers
 while True:
@@ -6,4 +7,6 @@ while True:
     if fib[-1] > 4000000:
         break
     if fib[-1] % 2 == 0:
-        print(fib[-1])
+        results.append(fib[-1])
+
+print(sum(results))
